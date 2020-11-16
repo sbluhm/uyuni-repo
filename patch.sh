770293fbb727992894c71615cc47e45d7320e9f3
sbluhm <stefan.bluhm@clacee.eu>
2020-11-16 12:13:12 +0100

    patch

--- patch.sh
+++ patch.sh
@@ -1,2 +1,5 @@
 curl https://raw.githubusercontent.com/sbluhm/uyuni/master/schema/spacewalk/spacewalk-schema-upgrade > /usr/bin/spacewalk-schema-upgrade
 curl https://raw.githubusercontent.com/sbluhm/uyuni/master/spacewalk/setup/bin/spacewalk-setup  > /usr/bin/spacewalk-setup
+curl https://raw.githubusercontent.com/sbluhm/uyuni/master/java/conf/default/rhn_taskomatic_daemon.conf  > /usr/share/rhn/config-defaults/rhn_taskomatic_daemon.conf
+curl https://raw.githubusercontent.com/sbluhm/uyuni/master/spacewalk/setup/share/tomcat.java_opts  > /usr/share/spacewalk/setup/tomcat.java_opts
+
