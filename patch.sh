8601014e6a4869e449201caba0caa28f409aa935
sbluhm <stefan.bluhm@clacee.eu>
2020-11-16 10:46:08 +0100

    patch

--- patch.sh
+++ patch.sh
@@ -1,2 +1,2 @@
 curl https://raw.githubusercontent.com/sbluhm/uyuni/master/schema/spacewalk/spacewalk-schema-upgrade > /usr/bin/spacewalk-schema-upgrade
-
+curl https://raw.githubusercontent.com/sbluhm/uyuni/master/spacewalk/setup/bin/spacewalk-setup  > /usr/bin/spacewalk-setup
