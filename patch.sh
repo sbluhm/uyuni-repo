9ea23712bc217bbbf51871e49182fd163ed97d8f
sbluhm <stefan.bluhm@clacee.eu>
2020-11-26 22:15:38 +0100

    Updated patch

--- patch.sh
+++ patch.sh
@@ -1,5 +1,7 @@
-curl https://raw.githubusercontent.com/sbluhm/uyuni/master/schema/spacewalk/spacewalk-schema-upgrade > /usr/bin/spacewalk-schema-upgrade
-curl https://raw.githubusercontent.com/sbluhm/uyuni/master/spacewalk/setup/bin/spacewalk-setup  > /usr/bin/spacewalk-setup
-curl https://raw.githubusercontent.com/sbluhm/uyuni/master/java/conf/default/rhn_taskomatic_daemon.conf  > /usr/share/rhn/config-defaults/rhn_taskomatic_daemon.conf
-curl https://raw.githubusercontent.com/sbluhm/uyuni/master/spacewalk/setup/share/tomcat.java_opts  > /usr/share/spacewalk/setup/tomcat.java_opts
-
+curl https://raw.githubusercontent.com/uyuni-project/uyuni/master/schema/spacewalk/spacewalk-schema-upgrade > /usr/bin/spacewalk-schema-upgrade
+curl https://raw.githubusercontent.com/uyuni-project/uyuni/master/spacewalk/setup/bin/spacewalk-setup  > /usr/bin/spacewalk-setup
+curl https://raw.githubusercontent.com/uyuni-project/uyuni/master/java/conf/default/rhn_taskomatic_daemon.conf  > /usr/share/rhn/config-defaults/rhn_taskomatic_daemon.conf
+curl https://raw.githubusercontent.com/uyuni-project/uyuni/master/spacewalk/setup/share/tomcat.java_opts  > /usr/share/spacewalk/setup/tomcat.java_opts
+dnf install glassfish-annotation-api glassfish-jaxb hibernate3
+#ln -s /usr/share/java/mchange-commons/mchange-commons-java.jar mchange-commons-java.jar
+#ln -s /usr/share/java/slf4j/slf4j-simple.jar slf4j-simple.jar 
