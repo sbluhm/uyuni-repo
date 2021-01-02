43bda4e321241723e43aa4b5a2253a4e39fd4614
sbluhm <stefan.bluhm@clacee.eu>
2021-01-02 17:42:01 +0100

    updated packages

--- patch.sh
+++ patch.sh
@@ -2,19 +2,11 @@ dnf -y install \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-java-4.2.5-1.el8.noarch.rpm \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-java-config-4.2.5-1.el8.noarch.rpm \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-taskomatic-4.2.5-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01851669-uyuni-base/uyuni-base-common-4.2.2-1.x86_64.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01851669-uyuni-base/uyuni-base-server-4.2.2-1.x86_64.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01851668-spacewalk-admin/spacewalk-admin-4.2.2-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01851667-perl-Satcon/perl-Satcon-4.2.1-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852268-spacewalk-setup/spacewalk-setup-4.2.3-1.el8.noarch.rpm
-
-chown :apache /etc/rhn
-
+	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852646-spacewalk-setup/spacewalk-setup-4.2.3-1.el8.noarch.rpm
+# java=tomcat libs, java-config=taskomatic conf, taskomatic=taskomatic libs, setup=tomcat conf
 
 # Workaround as http is not working.
+chown root:apache /etc/rhn
 sed -i 's#http://localhost#https://localhost#' /usr/bin/spacewalk-setup
 
 # Fixes waiting for merge:
-sed -i 's/--add-modules java.annotation,com.sun.xml.bind//' /usr/share/spacewalk/setup/tomcat.java_opts 
-sed -i 's/--add-modules java.annotation,com.sun.xml.bind//' /etc/sysconfig/tomcat
-cp -R /var/www/htdocs/* /var/www/html/
