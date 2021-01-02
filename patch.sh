b9c126fbe76812dc6343ca0da8faeaa241c298aa
sbluhm <stefan.bluhm@clacee.eu>
2021-01-02 12:58:33 +0100

    fixed url

--- patch.sh
+++ patch.sh
@@ -1,6 +1,6 @@
 dnf -y install \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-java-4.2.5-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572i-spacewalk-java/spacewalk-java-config-4.2.5-1.el8.noarch.rpm \
+	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-java-config-4.2.5-1.el8.noarch.rpm \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-taskomatic-4.2.5-1.el8.noarch.rpm \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01851669-uyuni-base/uyuni-base-common-4.2.2-1.x86_64.rpm \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01851669-uyuni-base/uyuni-base-server-4.2.2-1.x86_64.rpm \
