86e810a90543f8f534e8ef6f12556db301e12440
sbluhm <stefan.bluhm@clacee.eu>
2021-01-06 11:49:12 +0100

    url update

--- patch.sh
+++ patch.sh
@@ -3,7 +3,8 @@ dnf -y install \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-java-config-4.2.5-1.el8.noarch.rpm \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-taskomatic-4.2.5-1.el8.noarch.rpm \
 	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852646-spacewalk-setup/spacewalk-setup-4.2.3-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01864385-spacewalk-admin/spacewalk-admin-4.2.2-1.el8.noarch.rpm
+        https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01864414-spacewalk-admin/spacewalk-admin-4.2.2-1.el8.noarch.rpm
+
 # java=tomcat libs, java-config=taskomatic conf, taskomatic=taskomatic libs, setup=tomcat conf
 
 # Workaround as http is not working.
