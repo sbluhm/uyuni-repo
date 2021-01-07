2cc2e26bdc6da26e7b63082caf2cd1997d1a5afc
sbluhm <stefan.bluhm@clacee.eu>
2021-01-07 17:59:44 +0100

    removed

--- patch.sh
+++ patch.sh
@@ -1,15 +1,9 @@
 dnf -y install \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-java-4.2.5-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-java-config-4.2.5-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852572-spacewalk-java/spacewalk-taskomatic-4.2.5-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01852646-spacewalk-setup/spacewalk-setup-4.2.3-1.el8.noarch.rpm \
-        https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01864414-spacewalk-admin/spacewalk-admin-4.2.2-1.el8.noarch.rpm \
-	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01864577-uyuni-base/uyuni-base-common-4.2.1-1.x86_64.rpm
+	https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/01867280-spacewalk-config/spacewalk-config-4.2.2-1.el8.noarch.rpm
 
 # java=tomcat libs, java-config=taskomatic conf, taskomatic=taskomatic libs, setup=tomcat conf
 
 # Workaround as http is not working.
-chown root:apache /etc/rhn
 sed -i 's#http://localhost#https://localhost#' /usr/bin/spacewalk-setup
 
 # Fixes waiting for merge:
