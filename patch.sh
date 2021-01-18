a7ed4a44ccf272ce62328e7d4b9f0e23a1833d75
sbluhm <stefan.bluhm@clacee.eu>
2021-01-18 22:16:11 +0100

    updated patch

--- patch.sh
+++ patch.sh
@@ -7,4 +7,4 @@ sed -i 's#http://localhost#https://localhost#' /usr/bin/spacewalk-setup
 # Fixes waiting for merge:
 
 # reposync workaround until DNF works
-dnf -y install zypper
+#dnf -y install zypper
