0c2d17bf2e3ed2303bc2969e2eb6d4631e189547
sbluhm <stefan.bluhm@clacee.eu>
2021-07-09 12:58:35 +0200

    No fixes pending

--- patch.sh
+++ patch.sh
@@ -1,11 +1,6 @@
 #dnf -y install \
 #
 
-# Workaround as http is not working.
-#sed -i 's#http://localhost#https://localhost#' /usr/bin/spacewalk-setup
-sed -i /VirtualHost/d /etc/httpd/conf.d/cobbler.conf
 
 # Fixes waiting for merge:
 
-# reposync workaround until DNF works
-#dnf -y install zypper
