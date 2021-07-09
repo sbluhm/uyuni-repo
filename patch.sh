fc16dcba8d9ccebf89cf02294ead3e7979d73ae8
sbluhm <stefan.bluhm@clacee.eu>
2021-07-09 20:14:07 +0200

    Cobbler fixes for new OS

--- patch.sh
+++ patch.sh
@@ -4,3 +4,6 @@
 
 # Fixes waiting for merge:
 
+# cobbler fix for AlmaLinux/Rocky Linux.
+curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/cobbler/utils.py > /usr/lib/python3.6/site-packages/cobbler/utils.py
+curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/cobbler/distro_signatures.json > /var/lib/cobbler/distro_signatures.json
