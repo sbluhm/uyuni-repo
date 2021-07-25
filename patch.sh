4b18c37907887a51080bfd85bafa5d764a3a2843
sbluhm <stefan.bluhm@clacee.eu>
2021-07-25 13:25:33 +0200

    PR2994

--- patch.sh
+++ patch.sh
@@ -1,5 +1,7 @@
 #dnf -y install \
-#
+
+# Unclear what to do with these packages. Will keep them here for now:
+dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to requirements later
 
 
 # Fixes waiting for merge:
@@ -13,3 +15,6 @@ curl https://raw.githubusercontent.com/sbluhm/smdba/dev-eladaption/src/smdba/pos
 
 # https://github.com/uyuni-project/uyuni/pull/3988
 dnf -y install https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/02347846-spacewalk-selinux/spacewalk-selinux-2.8.6-1.el8.noarch.rpm
+
+# https://github.com/uyuni-project/uyuni/pull/3994
+dnf -y install https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/02348230-susemanager/susemanager-4.3.0-0.x86_64.rpm https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/02348230-susemanager/susemanager-tools-4.3.0-0.x86_64.rpm
