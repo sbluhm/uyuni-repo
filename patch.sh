7d8f3f283e5be9c8345c4a3c414cf211608a95fe
sbluhm <stefan.bluhm@clacee.eu>
2021-08-03 19:09:00 +0200

    Updated URLs

--- patch.sh
+++ patch.sh
@@ -8,10 +8,10 @@ dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to
 
 # cobbler fix for AlmaLinux/Rocky Linux.
 # Uyuni will start shipping cobbler 3.3. EPEL 8 must be updated with upstream 3.2
-curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/cobbler/utils.py > /usr/lib/python3.6/site-packages/cobbler/utils.py
-curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/cobbler/distro_signatures.json > /var/lib/cobbler/distro_signatures.json
+curl https://raw.githubusercontent.com/cobbler/cobbler/release32/cobbler/utils.py > /usr/lib/python3.6/site-packages/cobbler/utils.py
+curl https://raw.githubusercontent.com/cobbler/cobbler/release32/config/cobbler/distro_signatures.json > /var/lib/cobbler/distro_signatures.json
 
 # https://github.com/SUSE/smdba/pull/49
 # Waiting for release and integration into OBS
-curl https://raw.githubusercontent.com/sbluhm/smdba/dev-eladaption/src/smdba/postgresqlgate.py > /usr/lib/python3.6/site-packages/smdba/postgresqlgate.py
+curl https://raw.githubusercontent.com/SUSE/smdba/master/src/smdba/postgresqlgate.py > /usr/lib/python3.6/site-packages/smdba/postgresqlgate.py
 
