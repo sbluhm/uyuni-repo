557813fc07a4de1c558cbd291d66d14788f9bade
sbluhm <stefan.bluhm@clacee.eu>
2021-08-04 09:57:08 +0200

    smdba merged

--- patch.sh
+++ patch.sh
@@ -11,7 +11,4 @@ dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to
 curl https://raw.githubusercontent.com/cobbler/cobbler/release32/cobbler/utils.py > /usr/lib/python3.6/site-packages/cobbler/utils.py
 curl https://raw.githubusercontent.com/cobbler/cobbler/release32/config/cobbler/distro_signatures.json > /var/lib/cobbler/distro_signatures.json
 
-# https://github.com/SUSE/smdba/pull/49
-# Waiting for release and integration into OBS
-curl https://raw.githubusercontent.com/SUSE/smdba/master/src/smdba/postgresqlgate.py > /usr/lib/python3.6/site-packages/smdba/postgresqlgate.py
 
