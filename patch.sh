0074cc074a9ae056b36abb154a07dc3c04df2765
sbluhm <stefan.bluhm@clacee.eu>
2021-08-27 10:05:41 +0200

    Readding cobbler sync

--- patch.sh
+++ patch.sh
@@ -13,3 +13,9 @@ curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/tmp/rhn.conf > /
 # Uyuni will start shipping cobbler 3.3. EPEL 8 must be updated with upstream 3.2
 sed -i 's/("red hat", "redhat", "scientific linux", "fedora", "centos", "virtuozzo")/("red hat", "redhat", "scientific linux", "fedora", "centos", "virtuozzo", "almalinux", "rocky linux")/' /usr/lib/python3.6/site-packages/cobbler/utils.py
 sed -i 's/redhat|sl|slf|centos|oraclelinux|vzlinux/redhat|sl|slf|centos|oraclelinux|vzlinux|almalinux|rocky/' /var/lib/cobbler/distro_signatures.json
+
+
+# fix without patch yet:
+echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings
+service cobblerd restart
+cobbler sync
