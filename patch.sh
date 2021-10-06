1804175e5021e4626d59851013c0089d9ddda683
sbluhm <stefan.bluhm@clacee.eu>
2021-10-06 11:37:13 +0200

    cobbler fix

--- patch.sh
+++ patch.sh
@@ -3,6 +3,9 @@
 # Unclear what to do with these packages. Will keep them here for now:
 dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to requirements later
 
+# https://bugzilla.redhat.com/show_bug.cgi?id=2010567
+dnf -y install https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/02876112-cobbler/cobbler-3.2.2-1.el8.noarch.rpm
+
 
 # Fixes waiting for merge:
 # Cobbler setup fix #4166 
@@ -18,3 +21,7 @@ sed -i 's/redhat|sl|slf|centos|oraclelinux|vzlinux/redhat|sl|slf|centos|oracleli
 # fix without patch yet:
 echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings
 # /usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
+
+# Adding debug info
+echo "log_level_logfile: trace" > /etc/salt/master.d/logging.conf
+
