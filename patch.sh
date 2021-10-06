19486b2712b4583326da4d58e8818d011c4ed1fc
sbluhm <stefan.bluhm@clacee.eu>
2021-10-06 14:52:49 +0200

    removed cobbler fixes

--- patch.sh
+++ patch.sh
@@ -8,14 +8,9 @@ dnf -y install https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-s
 
 
 # Fixes waiting for merge:
-# Cobbler setup fix #4166 
-curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/tmp/rhn.conf > /usr/share/rhn/config-defaults/rhn.conf
-
 
 # cobbler fix for AlmaLinux/Rocky Linux.
-# Uyuni will start shipping cobbler 3.3. EPEL 8 must be updated with upstream 3.2
-sed -i 's/("red hat", "redhat", "scientific linux", "fedora", "centos", "virtuozzo")/("red hat", "redhat", "scientific linux", "fedora", "centos", "virtuozzo", "almalinux", "rocky linux")/' /usr/lib/python3.6/site-packages/cobbler/utils.py
-sed -i 's/redhat|sl|slf|centos|oraclelinux|vzlinux/redhat|sl|slf|centos|oraclelinux|vzlinux|almalinux|rocky/' /var/lib/cobbler/distro_signatures.json
+# Uyuni will start shipping cobbler 3.3. EPEL 8 must be updated with upstream 3.2.2
 
 
 # fix without patch yet:
