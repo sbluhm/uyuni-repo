04e8d58ba8b5e16bdc1959fcd47d2d660872afc0
sbluhm <stefan.bluhm@clacee.eu>
2021-10-15 12:24:51 +0200

    cleaned patch

--- patch.sh
+++ patch.sh
@@ -3,14 +3,8 @@
 # Unclear what to do with these packages. Will keep them here for now:
 dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to requirements later
 
-# https://bugzilla.redhat.com/show_bug.cgi?id=2010567
-dnf -y install https://download.copr.fedorainfracloud.org/results/sbluhm/uyuni-server/epel-8-x86_64/02876112-cobbler/cobbler-3.2.2-1.el8.noarch.rpm
-
 
 # Fixes waiting for merge:
-
-# cobbler fix for AlmaLinux/Rocky Linux.
-# Uyuni will start shipping cobbler 3.3. EPEL 8 must be updated with upstream 3.2.2
 
 
 # fix without patch yet:
