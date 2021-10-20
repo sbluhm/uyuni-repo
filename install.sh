6bb855c2d1cf77be0e7b8c49d8ecbb0fd94887d0
sbluhm <stefan.bluhm@clacee.eu>
2021-10-20 08:11:26 +0200

    Added install script

---
+++ install.sh
@@ -0,0 +1,25 @@
+set -e
+LANGPACK=glibc-langpack-de # UPDATE THIS TO THE LANGPACK OF YOUR CHOICE!
+dnf -y update
+dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm $LANGPACK # Installing EPEL and language pack
+
+REPO_SOURCE=https://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Master
+
+dnf -y config-manager --set-enabled powertools # required for dependencies
+rpm --import https://build.opensuse.org/projects/systemsmanagement:Uyuni:Master/public_key
+dnf -y config-manager --add-repo ${REPO_SOURCE}/AlmaLinux_8/
+dnf -y config-manager --add-repo ${REPO_SOURCE}:/Other/AlmaLinux_8/
+dnf -y config-manager --add-repo ${REPO_SOURCE}:/Other:/EL/AlmaLinux_8/
+
+dnf -y module enable postgresql:13 javapackages-tools cobbler pki-deps
+dnf -y module disable satellite-5-client rhn-tools # Don't use Spacewalk packages.
+
+NEWPACKAGES=$(curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/new-packages.txt)  # let's get packages that are waiting to be merged
+dnf -y install patterns-uyuni_server $NEWPACKAGES --enablerepo=epel-testing
+
+curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/root/setup_env.sh > /root/setup_env.sh
+curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/patch.sh | bash # Installs current fixes
+
+# Start the installation of the server now:
+/usr/lib/susemanager/bin/mgr-setup -s
+set +e
