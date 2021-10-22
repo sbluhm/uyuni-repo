81cd82600f2d2a3951ce2d58cdfa07871d223768
sbluhm <stefan.bluhm@clacee.eu>
2021-10-22 20:27:04 +0200

    spacewalk-setup cobbler fix

--- patch.sh
+++ patch.sh
@@ -5,11 +5,9 @@ dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to
 
 
 # Fixes waiting for merge:
-
+#ln -s /etc/cobbler/settings.yaml /etc/cobbler/settings
 
 # fix without patch yet:
-ln -s /etc/cobbler/settings.yaml /etc/cobbler/settings
-
 echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
 #cobbler sync
 # /usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
