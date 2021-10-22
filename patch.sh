63bbf03f7bad8724668e9c7d774cb9afdb2c7cc2
sbluhm <stefan.bluhm@clacee.eu>
2021-10-22 17:23:09 +0200

    cobbler 3.2.2 fixes

--- patch.sh
+++ patch.sh
@@ -8,8 +8,10 @@ dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to
 
 
 # fix without patch yet:
-echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings
-cobbler sync
+ln -s /etc/cobbler/settings.yaml /etc/cobbler/settings
+
+echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
+#cobbler sync
 # /usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
 
 # Adding debug info
