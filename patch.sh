592b8859dadc3f6db5b5390b52a26a386ad66dd9
sbluhm <stefan.bluhm@clacee.eu>
2021-12-04 18:01:40 +0100

    try mkgrub patch

--- patch.sh
+++ patch.sh
@@ -11,7 +11,7 @@ dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to
 # not sure if any patches are still required. seems to work without.
 #echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
 #cobbler sync
-# /usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
+/usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
 
 # Adding debug info
 echo "log_level_logfile: trace" >> /etc/salt/master.d/logging.conf
