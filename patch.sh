169504d6af9de7b3ba9f641c0421200946ab3662
sbluhm <stefan.bluhm@clacee.eu>
2021-12-04 18:23:06 +0100

    back to bootloader line

--- patch.sh
+++ patch.sh
@@ -9,9 +9,10 @@ dnf -y install py26-compat-tornado py26-compat-msgpack-python # will be added to
 # fix without patch yet:
 
 # not sure if any patches are still required. seems to work without.
-#echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
+echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
 #cobbler sync
-/usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
+#/usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
+#mkgrub tftp boots result in Failed to load libutil.c32 and menu.c32
 
 # Adding debug info
 echo "log_level_logfile: trace" >> /etc/salt/master.d/logging.conf
