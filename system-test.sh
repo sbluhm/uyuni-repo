aa4120782ddda7553f0f424ded59d0b7c149828f
sbluhm <stefan.bluhm@clacee.eu>
2021-12-17 22:26:20 +0100

    updated systemtest to /usr/bin/spacewalk-common-channels

--- system-test.sh
+++ system-test.sh
@@ -15,51 +15,22 @@ curl 'https://localhost/rhn/newlogin/CreateFirstUser.do' --insecure --data-raw '
 # Store credentials
 spacecmd -u admin -p admin -- whoami
 
-echo "Add AlmaLinux repositories"
-spacecmd -- repo_create --name "almalinux8-baseos-x86_64" --url "https://rsync.repo.almalinux.org/almalinux/8/BaseOS/x86_64/os/"
-spacecmd -- repo_create --name "almalinux8-appstream-x86_64" --url "https://rsync.repo.almalinux.org/almalinux/8/AppStream/x86_64/os/"
-spacecmd -- repo_create --name "almalinux8-powertools-x86_64" --url "https://rsync.repo.almalinux.org/almalinux/8/PowerTools/x86_64/os/"
-spacecmd -- repo_create --name "rhel8-epel-x86_64" --url "https://mirror.dogado.de/fedora-epel/8/Everything/x86_64/"
-spacecmd -- repo_create --name "el8-uyuni-client-tools-x86_64" --url "https://download.opensuse.org/repositories/systemsmanagement:/Uyuni:/Stable:/EL8-Uyuni-Client-Tools/EL_8/"
-
-
-echo "Add Channel structure"
-spacecmd -- softwarechannel_create -n "AlmaLinux 8 (x86_64)" -l "almalinux8-x86_64" -s "AlmaLinux 8 (x86_64)"
-spacecmd -- softwarechannel_create -n "AlmaLinux 8 BaseOS (x86_64)" -l "almalinux8-baseos-x86_64" -s "AlmaLinux 8 BaseOS (x86_64)" -p "almalinux8-x86_64"
-spacecmd -- softwarechannel_create -n "AlmaLinux 8 AppStream (x86_64)" -l "almalinux8-appstream-x86_64" -s "AlmaLinux 8 Appstream (x86_64)" -p "almalinux8-x86_64"
-#spacecmd -- softwarechannel_create -n "AlmaLinux 8 PowerTools (x86_64)" -l "almalinux8-powertools-x86_64" -s "AlmaLinux 8 PowerTools (x86_64)" -p "almalinux8-x86_64"
-spacecmd -- softwarechannel_create -n "EL 8 Uyuni Client Tools (x86_64)" -l "el8-uyuni-client-tools-x86_64" -s "EL 8 Uyuni Client Tools (x86_64)" -p "almalinux8-x86_64"
-#spacecmd -- softwarechannel_create -n "RHEL8 EPEL (x86_64)" -l "rhel8-epel-x86_64" -s "RHEL8 EPEL (x86_64)" -p "almalinux8-x86_64"
-
-echo "Configure channels"
-spacecmd -- softwarechannel_addrepo almalinux8-baseos-x86_64 almalinux8-baseos-x86_64
-spacecmd -- softwarechannel_addrepo almalinux8-appstream-x86_64 almalinux8-appstream-x86_64
-#spacecmd -- softwarechannel_addrepo almalinux8-powertools-x86_64 almalinux8-powertools-x86_64
-spacecmd -- softwarechannel_addrepo el8-uyuni-client-tools-x86_64 el8-uyuni-client-tools-x86_64
-#spacecmd -- softwarechannel_addrepo rhel8-epel-x86_64 rhel8-epel-x86_64
+# Create Repositories, Channels and Activation Keys
+/usr/bin/spacewalk-common-channels -u admin -p admin -k unlimited '*'
 
 echo "Synchronise Base channel"
 # Base needs to sync first or kickstart sync will get stuck
-spacecmd -- softwarechannel_syncrepos almalinux8-baseos-x86_64
+spacecmd -- softwarechannel_syncrepos almalinux8-x86_64
 
 echo "Create kickstartable tree"
-spacecmd -- softwarechannel_syncrepos almalinux8-baseos-x86_64 --sync-kickstart
+spacecmd -- softwarechannel_syncrepos almalinux8-x86_64 --sync-kickstart
 
 echo "Synchronise Autosetup repo dependency (AppStream)"
-spacecmd -- softwarechannel_syncrepos almalinux8-appstream-x86_64
-spacecmd -- softwarechannel_syncrepos el8-uyuni-client-tools-x86_64
-#spacecmd -- softwarechannel_syncrepos almalinux8-powertools-x86_64
-#spacecmd -- softwarechannel_syncrepos rhel8-epel-x86_64
+spacecmd -- softwarechannel_syncrepos almalinux8-x86_64-appstream 
+spacecmd -- softwarechannel_syncrepos almalinux8-uyuni-client-x86_64 
 
 
-echo "Creating Activation key"
-spacecmd -- activationkey_create -n "almalinux8-x86_64" -d "AlmaLinux 8 x86_64" -b "almalinux8-x86_64" -u
-## Configure key
-spacecmd -- activationkey_addchildchannels 1-almalinux8-x86_64 almalinux8-appstream-x86_64
-spacecmd -- activationkey_addchildchannels 1-almalinux8-x86_64 almalinux8-baseos-x86_64
-#spacecmd -- activationkey_addchildchannels 1-almalinux8-x86_64 almalinux8-powertools-x86_64
-spacecmd -- activationkey_addchildchannels 1-almalinux8-x86_64 el8-uyuni-client-tools-x86_64
-#spacecmd -- activationkey_addchildchannels 1-almalinux8-x86_64 rhel8-epel-x86_64
+echo "Configure Activation key"
 spacecmd -- activationkey_addentitlements 1-almalinux8-x86_64 ansible_control_node
 spacecmd -- activationkey_addentitlements 1-almalinux8-x86_64 container_build_host
 spacecmd -- activationkey_addentitlements 1-almalinux8-x86_64 monitoring_entitled
@@ -69,12 +40,14 @@ spacecmd -- activationkey_addentitlements 1-almalinux8-x86_64 virtualization_hos
 
 
 echo "Configure Autosetup distribution"
-until $( spacecmd -- distribution_update "almalinux8-baseos-x86_64" "-p /var/spacewalk/rhn/kickstart/1/almalinux8-baseos-x86_64" "-b almalinux8-baseos-x86_64" "-t rhel_8" );
+until $( spacecmd -- distribution_update "almalinux8-x86_64" "-p /var/spacewalk/rhn/kickstart/1/almalinux8-x86_64" "-b almalinux8-x86_64" "-t rhel_8" );
 do
 	sleep 60;
 done;
-spacecmd -- kickstart_create "-n almalinux8-x86_64" "-d almalinux8-baseos-x86_64" "-p admin" "-v none"
-echo 
+spacecmd -- kickstart_create "-n almalinux8-x86_64" "-d almalinux8-x86_64" "-p admin" "-v none"
+
+mgr-create-bootstrap-repo -c almalinux-8-x86_64-uyuni
+#sed -i 's/AppStream,//g' /var/spacewalk/rhn/kickstart/1/almalinux8-x86_64/.treeinfo
 
 # Test cobbler files
 dnf -y install tftp
@@ -85,6 +58,8 @@ get pxelinux.cfg/default
 get menu.c32
 get libutil.c32
 EOF
+
+# Somehow Autoinstall profile only works if distribution is manually saved
 
 echo "Done"
 
