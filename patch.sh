61aa08d4e7be05670a111ed0603d4fa31a06ca7d
sbluhm <stefan.bluhm@clacee.eu>
2022-04-01 11:34:53 +0200

    Updated headerlib URL and removed cobbler fixes

--- patch.sh
+++ patch.sh
@@ -2,7 +2,7 @@
 
 # Fixes waiting for merge:
 # PR4623
-curl https://raw.githubusercontent.com/sbluhm/uyuni/fix-4614/backend/server/importlib/headerSource.py -o /usr/lib/python3.6/site-packages/spacewalk/server/importlib/headerSource.py
+curl https://raw.githubusercontent.com/sbluhm/uyuni/fix-4614/python/spacewalk/server/importlib/headerSource.py -o /usr/lib/python3.6/site-packages/spacewalk/server/importlib/headerSource.py
 
 # Waiting for new python3-urlgrapper release (< 4.1.0-2)
 sed -i "1860s#self.fo = open(self.filename, 'r')#self.fo = open(self.filename, 'rb')#" /usr/lib/python3.6/site-packages/urlgrabber/grabber.py
@@ -12,16 +12,13 @@ sed -i "1860s#self.fo = open(self.filename, 'r')#self.fo = open(self.filename, '
 ln -s /var/lib/susemanager /srv/susemanager
 
 # not sure if any patches are still required. seems to work without.
-echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
+#echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
 #cobbler sync
 #/usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
 #mkgrub tftp boots result in Failed to load libutil.c32 and menu.c32
 
 # Adding debug info
 echo "log_level_logfile: trace" >> /etc/salt/master.d/logging.conf
-
-# Oracle Linux only patch until cobbler 3.3
-curl https://raw.githubusercontent.com/sbluhm/cobbler/2f85610c9865ed3f393e1e441eeab952d4de7d18/cobbler/utils.py -o /usr/lib/python3.6/site-packages/cobbler/utils.py
 
 # Random hack waiting to be fixed.
 #chmod a+w /var/log/rhn -R
