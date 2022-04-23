ad5cc0773ea85db9dfaf877a7beb3c561be03b1c
sbluhm <stefan.bluhm@clacee.eu>
2022-04-23 12:03:21 +0200

    re-added cobbler fix. removed 4614 merge

--- patch.sh
+++ patch.sh
@@ -1,8 +1,6 @@
 #dnf -y install \
 
 # Fixes waiting for merge:
-# PR4623
-curl https://raw.githubusercontent.com/sbluhm/uyuni/fix-4614/python/spacewalk/server/importlib/headerSource.py -o /usr/lib/python3.6/site-packages/spacewalk/server/importlib/headerSource.py
 
 # Waiting for new python3-urlgrapper release (< 4.1.0-2)
 sed -i "1860s#self.fo = open(self.filename, 'r')#self.fo = open(self.filename, 'rb')#" /usr/lib/python3.6/site-packages/urlgrabber/grabber.py
@@ -12,7 +10,7 @@ sed -i "1860s#self.fo = open(self.filename, 'r')#self.fo = open(self.filename, '
 ln -s /var/lib/susemanager /srv/susemanager
 
 # not sure if any patches are still required. seems to work without.
-#echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
+echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
 #cobbler sync
 #/usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
 #mkgrub tftp boots result in Failed to load libutil.c32 and menu.c32
