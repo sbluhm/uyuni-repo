272beba801c5e38bc0c3b1fb2bbef502ede0173f
sbluhm <stefan.bluhm@clacee.eu>
2022-04-27 20:02:58 +0200

    Added patch9

---
+++ patch9.sh
@@ -0,0 +1,22 @@
+#dnf -y install \
+
+# Fixes waiting for merge:
+
+# Waiting for new python3-urlgrapper release (< 4.1.0-2)
+sed -i "1860s#self.fo = open(self.filename, 'r')#self.fo = open(self.filename, 'rb')#" /usr/lib/python3.9/site-packages/urlgrabber/grabber.py
+
+# fix without patch yet:
+# Required whilst reworking folders
+ln -s /var/lib/susemanager /srv/susemanager
+
+# not sure if any patches are still required. seems to work without.
+echo "bootloaders_dir: '/usr/share/syslinux'" >> /etc/cobbler/settings.yaml
+#cobbler sync
+#/usr/share/cobbler/bin/mkgrub.sh # <-- should be this instead of cobbler sync. Code currently to be refactored and doesnt work anyways so only keeping for reference.
+#mkgrub tftp boots result in Failed to load libutil.c32 and menu.c32
+
+# Adding debug info
+echo "log_level_logfile: trace" >> /etc/salt/master.d/logging.conf
+
+# Random hack waiting to be fixed.
+#chmod a+w /var/log/rhn -R
