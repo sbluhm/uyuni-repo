3017f1e92bba2e73019446dfd6eaf9ca422f16b3
Stefan Bluhm <stefan.bluhm@clacee.eu>
2023-06-13 11:08:51 +0200

    Added symlink scan

--- system-test.sh
+++ system-test.sh
@@ -11,6 +11,9 @@ if [ "$1" = "install" ]; then
 # Use curl https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/system-test.sh | bash -s -- install
   echo "Installing Uyuni (low verbosity)"
   curl -s https://raw.githubusercontent.com/sbluhm/uyuni-repo/master/install9.sh | bash >> $LOG
+  echo "Testing for broken Symlinks"
+  find /usr/share/tomcat/webapps/rhn/WEB-INF/lib -xtype l
+  find /usr/share/spacewalk/taskomatic/ -xtype l
   /usr/lib/susemanager/bin/mgr-setup -s
 fi
 
