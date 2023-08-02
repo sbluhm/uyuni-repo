153261023e8cfd9b4812526e3d538ebec62d718f
Stefan Bluhm <stefan.bluhm@clacee.eu>
2023-08-02 10:08:44 +0200

    patch9.sh

--- patch9.sh
+++ patch9.sh
@@ -3,7 +3,8 @@
 sed -i "1860s#self.fo = open(self.filename, 'r')#self.fo = open(self.filename, 'rb')#" /usr/lib/python3.9/site-packages/urlgrabber/grabber.py
 ## BZ2188218: Update Java Softlinks for Taskomatik und Tomcat:
 ln -s /usr/share/tomcat/bin/tomcat-juli.jar /usr/share/java/tomcat/tomcat-juli.jar
-
+## BZ2224420: OpenJDK 11.0.20.0.8-1 does not include tzdata-java-2023c-1 [rhel-9, openjdk-11]
+dnf -y install tzdata-java-2023c
 
 
 # fix without patch yet:
