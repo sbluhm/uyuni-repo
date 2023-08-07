6a01e7cf93040524c29f2adc403b327eb6178072
Stefan Bluhm <stefan.bluhm@clacee.eu>
2023-08-07 20:24:50 +0200

    javasist

--- patch9.sh
+++ patch9.sh
@@ -3,8 +3,6 @@
 sed -i "1860s#self.fo = open(self.filename, 'r')#self.fo = open(self.filename, 'rb')#" /usr/lib/python3.9/site-packages/urlgrabber/grabber.py
 ## BZ2188218: Update Java Softlinks for Taskomatik und Tomcat:
 ln -s /usr/share/tomcat/bin/tomcat-juli.jar /usr/share/java/tomcat/tomcat-juli.jar
-## BZ2224420: OpenJDK 11.0.20.0.8-1 does not include tzdata-java-2023c-1 [rhel-9, openjdk-11]
-dnf -y install tzdata-java-2023c
 
 
 # fix without patch yet:
@@ -15,6 +13,8 @@ chown root:apache /var/log/rhn
 chmod 770 /var/log/rhn
 ##
 sed -i 's#/usr/bin/openssl genrsa -out %s 2048#/usr/bin/openssl genrsa -traditional -out %s 2048#' /usr/lib/python3.9/site-packages/certs/rhn_ssl_tool.py
+mkdir /usr/share/java/javassist/
+ln -s /usr/share/java/javassist.jar /usr/share/java/javassist/javassist.jar
 
 # Adding debug info
 #sed -i '/log_level_logfile/d' /etc/salt/master.d/logging.conf ||:
