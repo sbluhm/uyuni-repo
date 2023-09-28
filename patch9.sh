38d6653a5e0359a091a619aef7dec34faaa38f3e
Stefan Bluhm <stefan.bluhm@clacee.eu>
2023-09-28 21:55:50 +0200

    postscript does not run

--- patch9.sh
+++ patch9.sh
@@ -16,6 +16,12 @@ sed -i 's#/usr/bin/openssl genrsa -out %s 2048#/usr/bin/openssl genrsa -traditio
 mkdir /usr/share/java/javassist/
 ln -s /usr/share/java/javassist.jar /usr/share/java/javassist/javassist.jar
 
+## post script does not run: https://github.com/sbluhm/uyuni/blob/77c4ee91387b297d23d18e3d90a3b07bf1502082/spacewalk/setup/spacewalk-setup.spec#L194
+CURRENT_DATE=$(date +"%Y-%m-%dT%H:%M:%S.%3N")
+cp /etc/tomcat/server.xml /etc/tomcat/server.xml.$CURRENT_DATE
+xsltproc /usr/share/spacewalk/setup/server.xml.xsl /etc/tomcat/server.xml.$CURRENT_DATE > /etc/tomcat/server.xml
+
+
 # Adding debug info
 #sed -i '/log_level_logfile/d' /etc/salt/master.d/logging.conf ||:
 #echo "log_level_logfile: trace" >> /etc/salt/master.d/logging.conf
