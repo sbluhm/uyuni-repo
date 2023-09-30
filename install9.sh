0fc5b24f345a9e809ae286e8700effd11fcfcad2
Stefan Bluhm <stefan.bluhm@clacee.eu>
2023-09-30 07:15:47 +0200

    install from copr release by default

--- install9.sh
+++ install9.sh
@@ -2,6 +2,8 @@ set -ex
 if [ "$1" = "force" ]; then
   echo "Continue on error"
   set +e
+elif [ "$1" = "master" ]; then
+  master=1
 fi
 
 DISTRIBUTION_ID="$(source /etc/os-release && echo ${ID})"
@@ -33,10 +35,14 @@ if [ "$DISTRIBUTION_ID" = RHEL ] ; then
     REPO_SOURCE=https://download.opensuse.org/repositories/home:/sbluhm:/branches:/systemsmanagement:/Uyuni:/Master
     dnf -y config-manager --set-enabled crb ||: # required for dependencies
     dnf -y config-manager --set-enabled ol9_codeready_builder ||:  # Alternative for Oracle Linux. Ignore on all other systens,
-    rpm --import ${REPO_SOURCE}/EL_9/repodata/repomd.xml.key
-    dnf -y config-manager --add-repo ${REPO_SOURCE}/EL_9/
-    dnf -y config-manager --add-repo ${REPO_SOURCE}:/Other/EL_9/
-    dnf -y config-manager --add-repo ${REPO_SOURCE}:/Other:/EL/EL_9/
+    if [[ -v master ]]; then
+      rpm --import ${REPO_SOURCE}/EL_9/repodata/repomd.xml.key
+      dnf -y config-manager --add-repo ${REPO_SOURCE}/EL_9/
+      dnf -y config-manager --add-repo ${REPO_SOURCE}:/Other/EL_9/
+      dnf -y config-manager --add-repo ${REPO_SOURCE}:/Other:/EL/EL_9/
+    else
+      dnf -y copr enable sbluhm/uyuni-release
+    fi
     dnf -y config-manager --add-repo https://download.postgresql.org/pub/repos/yum/14/redhat/rhel-9-x86_64/
     rpm --import https://download.postgresql.org/pub/repos/yum/RPM-GPG-KEY-PGDG-14
     dnf -y update --nobest # to update python3-rhnlib on Oracle systems as it comes pre-installed.
